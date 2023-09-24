92e1ff1f8e1cbeda60ae484c965f93fecf23d203
elena1989 <elena.shilova89@gmail.com>
2023-09-24 21:33:56 +0300

    good

---
+++ interim_сertification/interim_certification.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+
+one_hot_data = pd.DataFrame()
+
+for category in data['whoAmI'].unique():
+    one_hot_data[category] = (data['whoAmI'] == category).astype(int)
+
+one_hot_data.head()
